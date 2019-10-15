2376fcabcf3e9ad3adbd90bc8958bc109e84547b
Rodrigo Cipriano <rccipriano@msn.com>
2019-10-15 11:22:07 -0300

    Primeiro Commit

---
+++ src/test/java/com/zup/EstudoAPI/EstudoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zup.EstudoAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstudoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
